ebf68b932fbcedf414d6aca2609898dd073bfd09
Momilijaz96 <momilijaz@gmail.com>
2022-04-11 12:37:03 -0400

    Update 0_graphs.py

--- 0_graphs.py
+++ 0_graphs.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 class Graph:
   def __init__(self, undirected):
     self.adjlist = {  };
@@ -18,16 +18,11 @@ class Graph:
     return self.adjlist;
 
 g = Graph(True);
-g.addEdge(3, 5);
-g.addEdge(5, 6);
-g.addEdge(5, 10);
-g.addEdge(6, 7);
-g.addEdge(6, 9);
-g.addEdge(7, 8);
-g.addEdge(10, 9);
-g.addEdge(9, 8);
-g.addEdge(8, 11);
-g.addEdge(10,12)
+g.addEdge(1,2)
+g.addEdge(2,3)
+g.addEdge(3,4)
+g.addEdge(4,5)
+g.addEdge(5,1)
 
 
 print(g.adjlist)

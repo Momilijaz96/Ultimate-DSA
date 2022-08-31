f42fef369423b089ca75b7e83df4ae27069b04e1
Momilijaz96 <momilijaz@gmail.com>
2022-08-31 19:58:27 +0500

    Update frogJumpK.py

--- DynamicProgrammig/frogJumpK.py
+++ DynamicProgrammig/frogJumpK.py
@@ -1,18 +1,21 @@
-def frogJumpK(h,i,k,dp):
-    if i==0:
-        return 0
-    if i>len(dp): #[i]==-1:
+def frogJumpK(h,idx,k):
+    dp = [0]
+    dp.append(abs(h[1]-h[0]))
+    for i in range(1,idx):
         mm_steps = float('inf')
         for j in range(1,k+1):
-            if i>=j:
-                if dp[i-j]!=-1:
-                    dp[i-j] = frogJumpK(h,i-j,k,dp)
-                mm_steps = min(mm_steps, dp[i-j] + abs(h[i]-h[i-j]))
-        dp[i] = mm_steps
-    return dp[i]
+            print(dp,i,j)
+            if i-j>=0: # and i-j<len(dp):
+                jmp = dp[i-j]
+                mm_steps = min(mm_steps, jmp + abs(h[i]-h[i-j]))
+        dp.append(mm_steps)
+        if len(dp)>k+1:
+            dp = dp[-k+1:]
+    return dp[-1]
 
 h = [40,10,20,70,80,10,20,70,80,60]
-dp = []
-res = frogJumpK(h,len(h)-1,4,dp) 
+k = 4
+#dp = [-1]*len(h)
+res = frogJumpK(h,len(h),k) 
 print(res)
 

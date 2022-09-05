ae714dffb37114f47dff77956535196b0fa1a5e9
Momilijaz96 <momilijaz@gmail.com>
2022-09-05 10:52:52 +0500

    Update unique_paths.py

--- DynamicProgrammig/unique_paths.py
+++ DynamicProgrammig/unique_paths.py
@@ -3,62 +3,60 @@ from sys import *
 from collections import *
 from math import *
 
-def f(x,y,m,n,ways):
-    if (x,y)==(m-1,n-1):
+def f_recur(x,y):
+    if x==0 and y==0:
         return 1
-    if x>m-1 or y>n-1: return 0
-    dwn = f(x+1,y,m,n,ways)
-    rit = f(x,y+1,m,n,ways)
-    ways += (dwn+rit)
-    return ways
+    if x<0 or y<0:
+        return 0
+    top = f_recur(x-1,y)
+    left = f_recur(x,y-1)
+    return top+left
 
-def f_mem(x,y,m,n,dp):
-    if (x,y)==(m-1,n-1):
+def f_mem(x,y,dp):
+    if x==0 and y==0:
         return 1
-    if x>m-1 or y>n-1: return 0
-    if dp[x][y]==0:
-        dwn = f_mem(x+1,y,m,n,dp)
-        rit = f_mem(x,y+1,m,n,dp)
-        dp[x][y] += (dwn+rit)
+    if x<0 or y<0:
+        return 0
+    if dp[x][y]==-1:
+        top = f_mem(x-1,y,dp)
+        left = f_mem(x,y-1,dp)
+        dp[x][y] = top+left
     return dp[x][y]
 
 def f_tab(m,n):
     dp = [[0]*n for _ in range(m)]
-    dp[0][0] = 1 #one way to reach start, btm-up
+    dp[0][0] = 1
     for i in range(m):
         for j in range(n):
-            if i-1>=0:
-                dp[i][j] += dp[i-1][j]
-            if j-1>=0:
-                dp[i][j] += dp[i][j-1]
+            if i+1<m:
+                dp[i+1][j] += dp[i][j]
+            if j+1<n:
+                dp[i][j+1] += dp[i][j]
     return dp[-1][-1]
 
 def f_sp(m,n):
-    top = 1
-    left = 1
-    for i in range(m):
+    row = [1]*n
+    for i in range(m-1):
+        next_row = [0]*n
         for j in range(n):
-            curr = 0
-            if i-1>=0:
-                curr += top
-                top = curr 
-            if j-1>=0:
-                curr += left
-                left = curr
-                top = curr
-    return curr
+            if i+1<m:
+                next_row[j] += row[j]
+            if j+1<n:
+                row[j+1] += row[j]
+        row = next_row.copy()
+    return row[-1]
 
 def uniquePaths(m, n):
-	# Recursion
-# 	return f(0,0,m,n,0)
-    
+    # Recursive solution
+    #res = f_recur(m-1,n-1)
+
     # Memoization
-#     dp = [[0]*n for _ in range(m)]
-#     res = f_mem(0,0,m,n,dp)
-    
+#     dp = [[-1]*n for _ in range(m)]
+#     res = f_mem(m-1,n-1,dp)
+
     # Tabulation
 #     res = f_tab(m,n)
 
-    #Space Optim
+    #Space Optimization
     res = f_sp(m,n)
     return res
